273e4f28f00bb51f6d385338cb3dbf8efa92a206
rbgtk <28433135+rbgtk@users.noreply.github.com>
2023-02-08 13:41:43 +0100

    storing hash and signature

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,6 +1,5 @@
 import * as fs from 'fs'
 import * as dotenv from 'dotenv'
-import axios, { AxiosResponse } from 'axios'
 import Web3 from 'web3'
 
 dotenv.config()
@@ -11,13 +10,29 @@ async function main():Promise<void> {
 
     const account = web3.eth.accounts.privateKeyToAccount(process.env.BC4P_PRIVKEY!)
 
-    const buffer = fs.readFileSync(process.argv[2]) // command line argument with path to data
+    const contract_address = process.env.BC4P_CONTRACT
 
-    const data = JSON.parse(buffer.toString())
+    const contract_json = JSON.parse(fs.readFileSync('../build/contracts/MessageRegistry.json').toString())
+
+    const contract = new web3.eth.Contract(contract_json.abi, contract_address)
+
+    const data = JSON.parse(fs.readFileSync(process.argv[2]).toString())
 
     const data_signature = account.sign(JSON.stringify(data))
     
-    
+    const transfer = contract.methods.storeHash(data_signature.messageHash, data_signature.signature).encodeABI()
+
+    const transaction = {
+        from: account.address,
+        to: contract_address,
+        gas: 750000,
+        data: transfer
+    }
+
+    account.signTransaction(transaction).then((transaction_signed) => {
+        web3.eth.sendSignedTransaction(transaction_signed.rawTransaction!)
+    })
+
 }
 
 main().catch(console.error)

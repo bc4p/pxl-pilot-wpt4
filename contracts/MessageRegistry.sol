1a54a4c6bdcfc3d0fb8a0614faa65b75d69bab28
rbgtk <28433135+rbgtk@users.noreply.github.com>
2023-02-08 09:53:52 +0100

    pragma version and event

--- contracts/MessageRegistry.sol
+++ contracts/MessageRegistry.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.8.17 <0.9.0;
 
 contract MessageRegistry {
   
@@ -15,7 +15,10 @@ contract MessageRegistry {
     registry[_hash].sender = msg.sender;
     registry[_hash].date = block.timestamp;
     registry[_hash].signature = _signature;
+
+    emit MessageStored(msg.sender, _hash);
   }
 
+  event MessageStored(address indexed _sender, bytes32 _hash);
   
 }
